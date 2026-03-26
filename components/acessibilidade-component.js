befbd8a6b94748529b8cb3ef9cb3fdd460ffc69c
ArianeArchanjo <ariane.archanjo1@gmail.com>
2026-03-26 12:36:58 -0300

    .

--- components/acessibilidade-component.js
+++ components/acessibilidade-component.js
@@ -582,7 +582,7 @@
                   <i class="fas fa-headphones" aria-hidden="true"></i>
                 </a>
 
-                <a href="/pages/acessibilidade/" class="barra-acesso-icone" title="Página de Acessibilidade">
+                <a href="../../pages/acessibilidade/" class="barra-acesso-icone" title="Página de Acessibilidade">
                   <i class="fas fa-universal-access" aria-hidden="true"></i>
                 </a>
               </div>
@@ -590,12 +590,12 @@
               <div class="barra-sep" aria-hidden="true"></div>
 
               <nav class="barra-badges" aria-label="Acesso rápido">
-                <a href="/pages/mapa-site/"
+                <a href="../../pages/mapa-site/"
                    aria-label="Mapa do site"
                    class="barra-acesso-icone" style="width:auto; padding:0 10px; font-size:10px; font-weight:700;">
                   <i class="fas fa-sitemap" aria-hidden="true"></i>&nbsp; MAPA DO SITE
                 </a>
-                <a href="/pages/e-sic/"
+                <a href="../../pages/e-sic/"
                    aria-label="Acesso à informação"
                    class="barra-acesso-icone" style="width:auto; padding:0 10px; font-size:10px; font-weight:700;">
                   <i class="fas fa-info-circle" aria-hidden="true"></i>&nbsp; ACESSO À INFORMAÇÃO
